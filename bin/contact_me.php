b52c1177ff8e12a110597c57f77c55014f3e434f
alegiuliani <a.giuliani@gmail.com>
2015-04-22 01:15:37 -0300

    Inicial

---
+++ bin/contact_me.php
@@ -0,0 +1,19 @@
+<?php
+if(empty($_POST['name']) || empty($_POST['phone']) || empty($_POST['email']) || empty($_POST['message']) || !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)) {
+	echo "Argumentos não enviados!";
+	return false;
+}
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email_address = $_POST['email'];
+$message = $_POST['message'];
+
+$to = 'microsutureindustria@gmail.com';
+$email_subject = "Envio de E-mail pelo site por:  $name";
+$email_body = "Contato enviado pelo site.\n\n"."Aqui estão os detalhes:\n\nNome: $name\n\nTelefone: $phone\n\nE-mail: $email_address\n\nMensagem:\n$message";
+$headers = "From: noreply@gmail.com\n";
+$headers .= "Reply-To: $email_address";	
+mail($to,$email_subject,$email_body,$headers);
+return true;			
+?>
---
+++ js/contact_me.js
@@ -0,0 +1,62 @@
+$(function() {
+
+    $("input,textarea").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function($form, event, errors) {
+        },
+        submitSuccess: function($form, event) {
+            event.preventDefault();
+            var name = $("input#name").val();
+            var phone = $("input#phone").val();
+            var email = $("input#email").val();
+            var message = $("textarea#message").val();
+            var firstName = name;
+            if (firstName.indexOf(' ') >= 0) {
+                firstName = name.split(' ').slice(0, -1).join(' ');
+            }
+            $.ajax({
+                url: "./bin/contact_me.php",
+                type: "POST",
+                data: {
+                    name: name,
+                    phone: phone,
+                    email: email,
+                    message: message
+                },
+                cache: false,
+                success: function() {
+                    $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-success')
+                        .append("<strong>Sua mensagem foi enviada com sucesso!</strong>");
+                    $('#success > .alert-success')
+                        .append('</div>');
+
+                    $('#contactForm').trigger("reset");
+                },
+                error: function() {
+                    $('#success').html("<div class='alert alert-danger'>");
+                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-danger').append("<strong>Desculpe " + firstName + " parece que o meu servidor de e-mail não está respondendo...</strong> Por favor, entre em contato diretamente no meu e-mail: <a href='mailto:ivan.microsuture@bol.com.br?Subject=Contato Site'>microsuture@microsuture.com.br</a> Desculpe pelo inconveniente!");
+                    $('#success > .alert-danger').append('</div>');
+
+                    $('#contactForm').trigger("reset");
+                },
+            })
+        },
+        filter: function() {
+            return $(this).is(":visible");
+        },
+    });
+
+    $("a[data-toggle=\"tab\"]").click(function(e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
+
+$('#name').focus(function() {
+    $('#success').html('');
+});
